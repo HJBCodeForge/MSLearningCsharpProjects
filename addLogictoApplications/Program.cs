c557fce167a523e453d86bd163e415281bbbc2ed
HJBCodeForge <henning@hjbcodeforge.com>
2025-10-29 08:12:14 -0400

    do while and continue loops

--- addLogictoApplications/Program.cs
+++ addLogictoApplications/Program.cs
@@ -278,22 +278,49 @@
 // foreach (var name in names) Console.WriteLine(name);
 
 //<--- Exercise - Complete a challenge activity using for and if statements --->
-for (int i = 1; i <= 100; i++)
+// for (int i = 1; i <= 100; i++)
+// {
+//     if (i % 3 == 0 && i % 5 == 0)
+//         Console.WriteLine($"{i} - FizzBuzz");
+//     else if (i % 3 == 0)
+//         Console.WriteLine($"{i} - Fizz");
+//     else if (i % 5 == 0)
+//         Console.WriteLine($"{i} - Buzz");
+//     else
+//         Console.WriteLine($"{i}");
+// }
+
+//<--- Add Looping Logic to Your Code Using the do-while and while Statements --->
+//<-- Exercise - Create do and while iteration loops -->
+// Random random = new Random();
+// int current = 0;
+
+// do
+// {
+//     current = random.Next(1, 11);
+//     Console.WriteLine(current);
+// } while (current != 7);
+
+//<-- Write a while statement that iterates only while a random number is greater than some value -->
+// Random random = new Random();
+// int current = random.Next(1, 11);
+
+// while (current >= 3)
+// {
+//     Console.WriteLine(current);
+//     current = random.Next(1, 11);
+// }
+// Console.WriteLine($"Last number: {current}");
+
+//<-- Use the continue statement to step directly to the Boolean expression -->
+Random random = new Random();
+int current = random.Next(1, 11);
+
+do
 {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        Console.WriteLine($"{i} - FizzBuzz");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine($"{i} - Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine($"{i} - Buzz");
-    }
-    else
-    {
-        Console.WriteLine($"{i}");
-    }
-}
+    current = random.Next(1, 11);
+    
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
--- addLogictoApplications/obj/Debug/net9.0/addLogictoApplications.AssemblyInfo.cs
+++ addLogictoApplications/obj/Debug/net9.0/addLogictoApplications.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("addLogictoApplications")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf82a7f7ba67958719ac468387a763b446493da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7339924b0ba30aa537305cedb558a4b0c090f0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("addLogictoApplications")]
 [assembly: System.Reflection.AssemblyTitleAttribute("addLogictoApplications")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
