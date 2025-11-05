8738737d96b9899820fd00d201b2277720114725
HJBCodeForge <henning@hjbcodeforge.com>
2025-11-05 15:21:36 -0500

    Exercise - Discover padding and alignment

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -728,3 +728,122 @@ I then writes the reversed sentance and the o count to the terminal*/
 //     Console.WriteLine($"{orderIds[i]}\t- Error");  
 //   }
 // }
+
+/*--- Format alphanumeric data for presentation in C# --- */
+// Use character escape sequences to add tabs, new lines, and Unicode characters to our strings.
+// Create verbatim string literals, and escape common characters like backslash ( \ ) and double-quotes ( "" ).
+// Merge templates with variables using composite formatting and string interpolation.
+// Include various format specifiers for percentages, currency, and numbers.
+
+//--- Exercise - Investigate string formatting basics ---
+
+// Composite Formatting?
+// string first = "Hello";
+// string second = "World";
+//string result = string.Format("{0} {1}", first, second);
+// Console.WriteLine("{1} {0}", first, second);
+// Console.WriteLine("{0} {0} {0}", first, second);
+
+//String interpolation?
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
+
+//Formatting currency
+// decimal price = 123.45m;
+// int discount = 50;
+// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+//Formatting numbers
+// decimal measurement = 123456.78912m;
+// Console.WriteLine($"Measurement: {measurement:N} units");
+// Console.WriteLine($"Measurement: {measurement:N4} units");
+
+//Formatting percentages
+//decimal tax = .36785m;
+//Console.WriteLine($"Tax rate: {tax:P2}");
+
+//Combining formatting approaches
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+
+// string yourDiscount = String.Format("You saved {0:C2}off the regular {1:C2} price. ", (price - salePrice), price);
+// Console.WriteLine(yourDiscount);
+// Console.WriteLine(yourDiscount += $"A discount of {(price - salePrice)/price:P2}!");
+
+//--- Exercise - Explore string interpolation ---
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// Console.WriteLine($"   Sahres: {productShares:N3} Product");
+// Console.WriteLine($"     Sub Total: {subtotal:C}");
+// Console.WriteLine($"           Tax: {taxPercentage:P2}");
+// Console.WriteLine($"     Total Billed: {total:C}");
+
+//--- Exercise - Discover padding and alignment ---
+/*
+Here's a brief list of categories of these built-in methods so you can get an idea of what's possible.
+
+Methods that add blank spaces for formatting purposes (PadLeft(), PadRight())
+Methods that compare two strings or facilitate comparison (Trim(), TrimStart(), TrimEnd(), GetHashcode(), the Length property)
+Methods that help you determine what's inside of a string, or even retrieve just a part of the string (Contains(), StartsWith(), EndsWith(), Substring())
+Methods that change the content of the string by replacing, inserting, or removing parts (Replace(), Insert(), Remove())
+Methods that turn a string into an array of strings or characters (Split(), ToCharArray()) 
+*/
+
+//Formatting strings by adding whitespace before or after
+//string input = "Pad this";
+// Console.WriteLine(input.PadLeft(12));
+// Console.WriteLine(input.PadRight(12));
+
+//What is an overloaded method?
+// Console.WriteLine(input.PadLeft(12, '-'));
+// Console.WriteLine(input.PadRight(12, '-'));
+
+//Working with padded strings
+// string paymentId = "769C";
+// string payeeName = "Mr. Stephen Ortega";
+// string paymentAmount = "$5,000.00";
+
+// var formattedLine = paymentId.PadRight(6);
+// formattedLine += payeeName.PadRight(24);
+// formattedLine += paymentAmount.PadLeft(10);
+
+// Console.WriteLine("1234567890123456789012345678901234567890");
+// Console.WriteLine(formattedLine);
+
+//--- Exercise - Complete a challenge to apply string interpolation to a form letter ---
+// string customerName = "Ms. Barros";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// Console.WriteLine($"{customerName}, \nAs a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+// Console.WriteLine($"\nCurrently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.");
+// Console.WriteLine($"\nOur new product, Glorious Future offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n");
+
+// Console.WriteLine("Here's a quick comparison:\n");
+
+// string comparisonMessage = "";
+
+// comparisonMessage = currentProduct.PadRight(20);
+// comparisonMessage += string.Format("{0:P}", currentReturn).PadRight(9);
+// comparisonMessage += string.Format("{0:C}", currentProfit);
+// comparisonMessage += "\n";
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += string.Format("{0:P}", newReturn).PadRight(9);
+// comparisonMessage += string.Format("{0:C}", newProfit);
+
+// Console.WriteLine(comparisonMessage);
